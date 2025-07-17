16339fe9599527521421681f1e17cad868369484
Omar Alispahic <omaralispahic@Omars-MacBook-Pro.local>
2025-07-17 19:42:28 +0200

    Added abstract wall class and two type of walls

--- walls.h
+++ walls.h
@@ -5,10 +5,42 @@
 #ifndef MICROENGINE_WALLS_H
 #define MICROENGINE_WALLS_H
 
-class Walls{
+class Wall{
 private:
     int xPos,yPos;
+    bool collidable;
+public:
+    Wall(int xPos, int yPos, bool collidable): xPos(xPos), yPos(yPos), collidable(collidable){}
 
+    bool isCollidable() const {return collidable;}
+
+    virtual int getX() const {return xPos;}
+
+    virtual int getY() const {return yPos;}
+
+    virtual char draw() const = 0;
+
+
+};
+
+class SolidWall : public Wall{
+
+public:
+    SolidWall(int xPos, int yPos): Wall(xPos,yPos,true){}
+
+    char draw() const override {
+        return '#';
+    }
+
+};
+
+class GhostWall : public Wall{
+public:
+    GhostWall(int xPos, int yPos): Wall(xPos,yPos,false){}
+
+    char draw() const override{
+        return '+';
+    }
 };
 
 #endif //MICROENGINE_WALLS_H
